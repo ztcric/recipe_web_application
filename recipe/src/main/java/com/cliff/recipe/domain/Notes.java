bab100857495c5b4eacbc0622e92bf335bd4ff1b
Cliff <tzcliff@outlook.com>
2020-02-28 16:28:04 -0500

    Added Bootstrap CSS

--- recipe/src/main/java/com/cliff/recipe/domain/Notes.java
+++ recipe/src/main/java/com/cliff/recipe/domain/Notes.java
@@ -1,10 +1,12 @@
 package com.cliff.recipe.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
